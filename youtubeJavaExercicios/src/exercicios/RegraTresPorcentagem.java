10e07886110fcd3a40226edfcb3db5f825dd90c3
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 17:52:54 -0300

    Exercício - Regra de 3

---
+++ youtubeJavaExercicios/src/exercicios/RegraTresPorcentagem.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class RegraTresPorcentagem {
+
+	public static void main(String[] args) {
+		/* Desenvolva uma aplicação no console (terminal) para calcular
+		 * o valor da porcentagem usando Regra de 3, conforme modelo abaixo:
+		 * 
+		 * x% de y = valor
+		 * 
+		 * Exemplo: 10% de 200 = 20
+		*/
+	}
+
+}
