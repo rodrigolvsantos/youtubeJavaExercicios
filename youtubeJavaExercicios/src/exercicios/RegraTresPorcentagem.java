3bc031055c503900dd70f9346e179fdb3d13cff4
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 18:15:26 -0300

    Resolvendo exercicio regra de 3 com porcentagem.

--- youtubeJavaExercicios/src/exercicios/RegraTresPorcentagem.java
+++ youtubeJavaExercicios/src/exercicios/RegraTresPorcentagem.java
@@ -1,5 +1,8 @@
 package exercicios;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class RegraTresPorcentagem {
 
 	public static void main(String[] args) {
@@ -10,6 +13,25 @@ public class RegraTresPorcentagem {
 		 * 
 		 * Exemplo: 10% de 200 = 20
 		*/
+		
+		double x, y, resto, resultado;
+		Scanner teclado = new Scanner(System.in);
+		
+		DecimalFormat df = new DecimalFormat("0");
+		
+		System.out.print("Digite o valor de X: ");
+		x = teclado.nextDouble();
+		
+		System.out.print("Digite o valor de Y: ");
+		y = teclado.nextDouble();
+		
+		resto = x / 100;
+		resultado = resto * y;
+		
+		System.out.println(df.format(x)+"% de "+df.format(y) + " é : " + df.format(resultado));
+		//System.out.println(resto);
+		teclado.close();
+		
 	}
 
 }
