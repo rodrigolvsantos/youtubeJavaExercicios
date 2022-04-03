9878526656df105b2c7a0385dfbd239313cabe38
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 18:27:10 -0300

    Regra de 3 resolvida e código Comitado

--- youtubeJavaExercicios/src/exercicios/RegraTresPorcentagem.java
+++ youtubeJavaExercicios/src/exercicios/RegraTresPorcentagem.java
@@ -6,32 +6,33 @@ import java.util.Scanner;
 public class RegraTresPorcentagem {
 
 	public static void main(String[] args) {
-		/* Desenvolva uma aplicação no console (terminal) para calcular
-		 * o valor da porcentagem usando Regra de 3, conforme modelo abaixo:
+		/*
+		 * Desenvolva uma aplicação no console (terminal) para calcular o valor da
+		 * porcentagem usando Regra de 3, conforme modelo abaixo:
 		 * 
 		 * x% de y = valor
 		 * 
 		 * Exemplo: 10% de 200 = 20
-		*/
-		
+		 */
+
 		double x, y, resto, resultado;
 		Scanner teclado = new Scanner(System.in);
-		
+
 		DecimalFormat df = new DecimalFormat("0");
-		
+
 		System.out.print("Digite o valor de X: ");
 		x = teclado.nextDouble();
-		
+
 		System.out.print("Digite o valor de Y: ");
 		y = teclado.nextDouble();
-		
+
 		resto = x / 100;
 		resultado = resto * y;
-		
-		System.out.println(df.format(x)+"% de "+df.format(y) + " é : " + df.format(resultado));
-		//System.out.println(resto);
+
+		System.out.println(df.format(x) + "% de " + df.format(y) + " é : " + df.format(resultado));
+		// System.out.println(resto);
 		teclado.close();
-		
+
 	}
 
 }
