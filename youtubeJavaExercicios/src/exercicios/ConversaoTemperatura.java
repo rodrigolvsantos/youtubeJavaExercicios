b4f8f39300edfa8071d5153a8021c861c8711627
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 17:48:11 -0300

    Resolvendo o exercicio de transformar fahrenheit em celsius

--- youtubeJavaExercicios/src/exercicios/ConversaoTemperatura.java
+++ youtubeJavaExercicios/src/exercicios/ConversaoTemperatura.java
@@ -1,10 +1,27 @@
 package exercicios;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ConversaoTemperatura {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Variaveis
+		double c,f;
+		//Objetos
+		Scanner teclado = new Scanner(System.in);
+		DecimalFormat formatador = new DecimalFormat("#0.0");
+		//entrada de dados
+		System.out.println("Conversao de temperatura");
+		System.out.print("Digite a temperatura em fahrenheit: ");
+		f = teclado.nextDouble();
+		//processamento
+		c = (5 * (f - 32)) / 9;
+		//saida 
+		System.out.println("Temperatura em Celsius: " + formatador.format(c) + " 	°C");
+		teclado.close();
+		
+		
 	}
 
 }
