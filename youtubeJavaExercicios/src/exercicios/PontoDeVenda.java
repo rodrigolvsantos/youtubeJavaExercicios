16276af2077637057bbe67f3304346f7144014b8
rodrigo <rodrigo@localhost>
2022-04-03 19:06:39 -0300

    Corrigido problema de uncode no projeto - mudado para utf-8

--- youtubeJavaExercicios/src/exercicios/PontoDeVenda.java
+++ youtubeJavaExercicios/src/exercicios/PontoDeVenda.java
@@ -4,16 +4,16 @@ public class PontoDeVenda {
 
 	public static void main(String[] args) {
 		/*PONTO DE VENDA
-		 * Desenvolva uma aplicaÁ„o no console (terminal) de ponto de vendas (PDV),
+		 * Desenvolva uma aplica√ß√£o no console (terminal) de ponto de vendas (PDV),
 		 * conforme modelo abaixo:
 		 * 
 		 * Valor total: 200,00
 		 * Desconto (%): 5
-		 * Total desconto: 190,00 (valor j· calculado)
+		 * Total desconto: 190,00 (valor j√° calculado)
 		 * Valor Pago: 200,00
 		 * Troco: 10,00 (valor calculado)
 		 * 
-		 * Ps. D· para criar uma classe de desconto e usar nos dois exercÌcios!!
+		 * Ps. D√° para criar uma classe de desconto e usar nos dois exerc√≠cios!!
 		 */
 	}
 
