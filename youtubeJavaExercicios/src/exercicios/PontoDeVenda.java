621f5e5cbe22a5bb32b0b4c794cedb4314e4edb9
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 18:33:06 -0300

    Exercício - Ponto de vendas!

---
+++ youtubeJavaExercicios/src/exercicios/PontoDeVenda.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class PontoDeVenda {
+
+	public static void main(String[] args) {
+		/*PONTO DE VENDA
+		 * Desenvolva uma aplicação no console (terminal) de ponto de vendas (PDV),
+		 * conforme modelo abaixo:
+		 * 
+		 * Valor total: 200,00
+		 * Desconto (%): 5
+		 * Total desconto: 190,00 (valor já calculado)
+		 * Valor Pago: 200,00
+		 * Troco: 10,00 (valor calculado)
+		 * 
+		 * Ps. Dá para criar uma classe de desconto e usar nos dois exercícios!!
+		 */
+	}
+
+}
