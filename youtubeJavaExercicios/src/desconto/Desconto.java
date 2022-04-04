c46d684d79a0cb4d314358cc793aae39aeec5a75
rodrigo <rodrigo@localhost>
2022-04-04 17:12:55 -0300

    Resolucao exercicio - PDV - Criacao da classe Deconto

---
+++ youtubeJavaExercicios/src/desconto/Desconto.java
@@ -0,0 +1,41 @@
+package desconto;
+
+import java.text.DecimalFormat;
+
+public class Desconto {
+
+	 private static final DecimalFormat df = new DecimalFormat("0.00");
+
+	private double x;
+	private double y;
+	private double resultado;
+
+	public double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	double desconto (double x, double y) {
+		resultado = (x * y) / 100;
+		return resultado;
+	}
+}
