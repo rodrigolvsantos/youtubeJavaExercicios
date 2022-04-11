77bb8538cab27fb868131422662f251e3e754f35
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-10 21:59:10 -0300

    Imposto Calculado

--- youtubeJavaExercicios/src/exercicios/CalculeServico.java
+++ youtubeJavaExercicios/src/exercicios/CalculeServico.java
@@ -1,10 +1,30 @@
 package exercicios;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-public class CalculeServico {
+import desconto.Desconto;
+
+public class CalculeServico extends Desconto{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		CalculeServico servico = new CalculeServico();
+		double remuneraMes, calculaImposto, custoOperacional, investimento, desconto, horaTrabalhada, taxaImposto, taxaInvestimento;
+
+		taxaImposto = 30;
+
+		Scanner teclado = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0");
+
+		System.out.print("Digite o salário por mês: ");
+		remuneraMes = teclado.nextDouble();
+
+		servico.setY(remuneraMes);
+		servico.setX(taxaImposto);
+
+		calculaImposto = servico.desconto();		
+		System.out.println(calculaImposto);
 	}
 
 }
